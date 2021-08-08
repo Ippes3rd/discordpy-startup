02749ae3c9605a93b475e98ffaafbfd183e46e32
Ippes3rd <88631605+Ippes3rd@users.noreply.github.com>
2021-08-09 05:31:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,45 +8,16 @@ CHANNEL_ID = 873990257375318107 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-
-#投稿する日時
-dateTimeList = [
-'2021/08/09 05:30',
-]
-
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    print('ready')
-
-# 指定時間に走る処理
-async def SendMessage():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')
-
-# 30秒に一回ループ
-@tasks.loop(seconds=30)
-async def time_check():
-    sleepTime = 0
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
     # 現在の時刻
-    now = datetime.now().strftime('%Y/%m/%d %H:%M')
-    if now in dateTimeList :
-        print(now)
-        await SendMessage()
-        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
-        await asyncio.sleep(30)
+    now = datetime.now().strftime('%H:%M')
+    if now == '07:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 使用できるコマンド一覧
-    if message.content == '!help':
-        await message.channel.send('現在使用できるコマンドはありません')
-
-#ループ処理
-time_check.start()
+#ループ処理実行
+loop.start()
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
