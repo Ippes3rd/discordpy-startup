17ccad5ec6b8723c9ab9b383cfea5fe1a778a4b4
Ippes3rd <88631605+Ippes3rd@users.noreply.github.com>
2021-08-09 05:14:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord.ext import tasks
 import os
 import traceback
+from datetime import datetime 
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
