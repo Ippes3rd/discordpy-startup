e28656e6ae1ff3bbb08248b748bca8a65b152574
Ippes3rd <88631605+Ippes3rd@users.noreply.github.com>
2021-08-09 05:27:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,29 +1,52 @@
-from discord.ext import commands
-import os
-import traceback
+#coding:UTF-8
+import discord
+from datetime import datetime
+from discord.ext import tasks
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = '#test' #チャンネルID
+CHANNEL_ID = 873990257375318107 #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#投稿する日時
+dateTimeList = [
+'2021/08/09 05:30',
+]
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    print('ready')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# 指定時間に走る処理
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
 
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-    
-@bot.command()
-async def inu(ctx):
-    await ctx.send('（Ｕ＾ω＾）わんわんお！')    
-    
-bot.run(token)
+# 30秒に一回ループ
+@tasks.loop(seconds=30)
+async def time_check():
+    sleepTime = 0
+    # 現在の時刻
+    now = datetime.now().strftime('%Y/%m/%d %H:%M')
+    if now in dateTimeList :
+        print(now)
+        await SendMessage()
+        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
+        await asyncio.sleep(30)
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 使用できるコマンド一覧
+    if message.content == '!help':
+        await message.channel.send('現在使用できるコマンドはありません')
+
+#ループ処理
+time_check.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
